9c74e10699943a12d794fa25c3616a4f1dcd8384
SidharthK2 <sidharthkapoor.100@gmail.com>
2022-09-24 00:36:06 +0530

    update config for goerli

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,62 @@ require("dotenv").config();
 require("solidity-coverage");
 require("hardhat-deploy");
 
+const GOERLI_RPC_URL =
+  process.env.GOERLI_RPC_URL || "https://www.ethercluster.com/goerli";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "key";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+    goerli: {
+      chainId: 5,
+      blockConfirmations: 6,
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+      },
+      {
+        version: "0.4.19",
+      },
+    ],
+  },
+  mocha: {
+    timeout: 500000,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
+  gasReporter: {
+    enabled: false,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
+  etherscan: {
+    // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+    apiKey: {
+      rinkeby: ETHERSCAN_API_KEY,
+    },
+  },
 };
